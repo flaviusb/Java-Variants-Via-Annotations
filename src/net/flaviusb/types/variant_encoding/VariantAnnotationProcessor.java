8be41c7acbef51bf8bea9bf2f6d56488d113459b
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2016-12-07 00:43:02 +1300

    Annotate processor.

--- src/net/flaviusb/types/variant_encoding/VariantAnnotationProcessor.java
+++ src/net/flaviusb/types/variant_encoding/VariantAnnotationProcessor.java
@@ -3,6 +3,8 @@ package net.flaviusb.types.variant_encoding;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
@@ -10,6 +12,10 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
 import java.util.Set;
 
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+@SupportedAnnotationTypes({
+   "net.flaviusb.types.variant_encoding.Variant"
+ })
 public class VariantAnnotationProcessor extends AbstractProcessor {
   @Override
   public boolean process(Set<? extends TypeElement> base, RoundEnvironment env) {
