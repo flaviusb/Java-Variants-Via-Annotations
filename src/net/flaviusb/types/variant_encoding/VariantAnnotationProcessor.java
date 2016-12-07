6129db0c42c835f8df3d5164097b81603ad13d77
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2016-12-07 23:34:56 +1300

    Make groups of VariantInstances, grouped by variant base name.

--- src/net/flaviusb/types/variant_encoding/VariantAnnotationProcessor.java
+++ src/net/flaviusb/types/variant_encoding/VariantAnnotationProcessor.java
@@ -11,14 +11,35 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedAnnotationTypes({
    "net.flaviusb.types.variant_encoding.Variant"
  })
 public class VariantAnnotationProcessor extends AbstractProcessor {
+  static class VariantInstance {
+    VariantInstance(String facade, String implementation) {
+      facadeName = facade;
+      implementationName = implementation;
+    }
+    String facadeName;
+    String implementationName;
+  }
   @Override
-  public boolean process(Set<? extends TypeElement> base, RoundEnvironment env) {
+  public boolean process(Set<? extends TypeElement> variant_classes, RoundEnvironment env) {
+    // Transform the unsorted annotations into VariantInstances grouped by baseName
+    Map<String, List<VariantInstance>> variant_groups = new HashMap<String, List<VariantInstance>>();
+    for(TypeElement element : variant_classes) {
+      Variant v = element.getAnnotation(Variant.class);
+      List<VariantInstance> group = variant_groups.getOrDefault(v.baseName(), new ArrayList<VariantInstance>());
+      group.add(new VariantInstance(v.facadeName(), element.getQualifiedName().toString()));
+      variant_groups.put(v.baseName(), group);
+    }
     return false;
   }
 }
