4c86f7b95ede9cddf1a2b96ccaaaaad649033e84
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2016-12-12 00:49:59 +1300

    Generate base variant interface, stash writers inside VariantInstances.

--- src/net/flaviusb/types/variant_encoding/VariantAnnotationProcessor.java
+++ src/net/flaviusb/types/variant_encoding/VariantAnnotationProcessor.java
@@ -27,12 +27,14 @@ import java.io.Writer;
  })
 public class VariantAnnotationProcessor extends AbstractProcessor {
   static class VariantInstance {
-    VariantInstance(String facade, String implementation) {
+    VariantInstance(String facade, String implementation, Writer handle) {
       facadeName = facade;
       implementationName = implementation;
+      source_handle = handle
     }
     String facadeName;
     String implementationName;
+    Writer source_handle;
   }
   Filer filer;
   @Override public synchronized void init(ProcessingEnvironment env) {
@@ -46,16 +48,37 @@ public class VariantAnnotationProcessor extends AbstractProcessor {
     for(TypeElement element : variant_classes) {
       Variant v = element.getAnnotation(Variant.class);
       List<VariantInstance> group = variant_groups.getOrDefault(v.baseName(), new ArrayList<VariantInstance>());
-      group.add(new VariantInstance(v.facadeName(), element.getQualifiedName().toString()));
+      group.add(new VariantInstance(v.facadeName(), element.getQualifiedName().toString(), filer.createSourceFile(v.facadeName()).openWriter()));
       variant_groups.put(v.baseName(), group);
     }
-    // Generate base interface for each variant
     for(String variant_base_name : variant_groups.keySet()) {
-       try {
-         Writer variant_base_class_output = filer.createSourceFile(variant_base_name).openWriter();
-       } catch (java.io.IOException e) {
-         throw new Error(e);
-       }
+      try {
+        // Generate base interface for each variant
+        List<VariantInstance> variants = variant_groups.get(variant_base_name);
+        Writer variant_base_class_output = filer.createSourceFile(variant_base_name).openWriter();
+        StringBuilder sb = new StringBuilder();
+        if(variant_base_name.contains(".")) {
+          String[] parts = variant_base_name.split("[.](?=[^.]+$)");
+          sb.append("package ");
+          sb.append(parts[0]);
+          sb.append(";\n\npublic interface ");
+          sb.append(parts[1]);
+        } else {
+          sb.append("public interface ");
+          sb.append(variant_base_name);
+        }
+        sb.append(" {\n");
+        for(VariantInstance vi : variants) {
+          sb.append("public Optional<");
+          sb.append(vi.facadeName);
+          sb.append("> get);
+          sb.append(vi.facadeName);
+          sb.append("();\n");
+        }
+        sb.append("}\n");
+      } catch (java.io.IOException e) {
+        throw new Error(e);
+      }
     }
     return false;
   }
